b969f4834a6e75f5bad1f2bd2e952f661776959e
Brian <bdrakester@gmail.com>
2019-04-25 21:24:41 -0700

    Adding comments.

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,3 +1,9 @@
+/**
+ * The Percolation class estimates the value of the percolation threshold via Monte Carlo simulation.
+ *  
+ * @author Brian Drake
+ * 
+ */
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
